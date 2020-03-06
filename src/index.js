9065e4f7272c1b3713cf318feb27b8a3ba50f88f
Karl Gong <karl.gong@outlook.com>
2020-03-06 10:25:12 +0800

    test

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@
 import "./css/main.css";
 
 // Say hello
-console.log("🦊 Hello! Edit me in src/index.js");
+console.log("Hello! Edit me in src/index.js");
--- src/js/cms.js
+++ src/js/cms.js
@@ -1,14 +1,7 @@
-import React from "react";
 import CMS from "netlify-cms-app";
 
 // Import main site styles as a string to inject into the CMS preview pane
 import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../css/main.css";
-
-import HomePreview from "./cms-preview-templates/home";
-import PostPreview from "./cms-preview-templates/post";
-import ProductsPreview from "./cms-preview-templates/products";
-import ValuesPreview from "./cms-preview-templates/values";
-import ContactPreview from "./cms-preview-templates/contact";
 
 CMS.registerPreviewStyle(styles, { raw: true });
 CMS.init();

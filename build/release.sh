82260e11cd4d69b4827ae57106c40e2bb098bb4c
Bill Zhuang <bill@billzhuang.com>
2020-03-29 17:59:45 +0800

    fix git pull issue

--- build/release.sh
+++ build/release.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+cd /opt/website
+
+git pull --rebase
+
+npm run build
+
 rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /opt/travis_rsa" --progress /opt/website/dist/ root@47.101.163.89:/opt/website2/public/ --delete
